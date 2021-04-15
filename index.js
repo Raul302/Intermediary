01eccdde2ec8669f8f1186c2346cb053c1f7652c
Raul302 <luis_1547@hotmail.com>
2021-04-15 15:17:49 -0500

    up

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
+// APP JS 
+
 const express = require('express');
 const app = express();
 
-app.get('/',(req,res) => res.send('Im working!'));
+const cors = require('cors');
+
+require('dotenv').config()
+require('./db');
+app.use(cors());
+
+app.get('/',(req,res) =>{
+    res.send('register');
+// Si ya iniciamos Session 
+
+// Si no iniciamos Session 
+
+// Pantallas de muestra 
+});
+app.post('/login',(req,res) => {
+//    Recibir credenciales
+
+
+});
+
+app.post('/register',(req,res) => {
+    res.send('register')
+});
 
 app.listen(3000);
 console.log('Put attention,Server on Port',3000);
---
+++ models/users.js
@@ -0,0 +1,12 @@
+const getAll = () => {
+    return new Promise((resolve,reject) => {
+        db.query('SELECT * FROM users',(err,rows) => {
+            if(err) reject(err)
+            resolve(rows);
+        });
+    });
+};
+
+module.exports = {
+    getAll: getAll
+}
